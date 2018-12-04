15f1b4ab41efec646945d35af5feeaced44cc73c
Chad Luong <apple.luong1905@gmail.com>
2018-12-04 19:50:38 +0700

    register form

--- session15/register.php
+++ session15/register.php
@@ -2,24 +2,45 @@
 <html>
 <head>
 	<title>Form register</title>
+	<style type="text/css">
+		span {
+			color: red;
+		}
+	</style>
 </head>
 <body>
 	<?php 
+	$errName = $errUsername = $errPass = '';
+	$name = $username = $password = '';
 	if(isset($_POST['register'])) {
-		echo "<pre>";
-		//var_dump($_POST);
-		//isset();
-		var_dump($_POST['name']);
-	} else {
-		echo "chuaw submit";
-	}
-		
+		$name     = $_POST['name'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		if($name == '') {
+			$errName = 'Please input your name';
+		}
+		if($username == '') {
+			$errUsername = 'Please input your username';
+		}
+		if($password == '') {
+			$errPass = 'Please input your password';
+		}
+	} 	
 	?>
 	<h1>Register form</h1>
 	<form action="#" method="post">
-		<p>Name:<input type="text" name="name"></p>
-		<p>Username:<input type="text" name="username"></p>
-		<p>Password:<input type="password" name="password"></p>
+		<p>Name:<input type="text" name="name" 
+		value="<?php echo $name?>">
+			<span><?php echo $errName;?></span>
+		</p>
+		<p>Username:<input type="text" name="username"
+		value="<?php echo $username?>">
+			<span><?php echo $errUsername;?></span>
+		</p>
+		<p>Password:<input type="password" name="password"
+		value="<?php echo $password?>">
+			<span><?php echo $errPass;?></span>
+		</p>
 		<input type="submit" name="register" value="Register">
 	</form>
 </body>
