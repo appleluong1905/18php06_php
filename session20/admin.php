fd8b540e48d6ba5cf0a2abdba6e2a289082003d7
Chad Luong <apple.luong1905@gmail.com>
2019-01-15 18:33:06 +0700

    update mvc oop final

---
+++ session20/admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Shop Management</title>
+	<link rel="stylesheet" type="text/css" href="public/css/backend.css">
+</head>
+<body>
+	<h1>Backend</h1>
+</body>
+</html>
