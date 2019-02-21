72057021348e8148325dec442f1824936c460e26
Chad Luong <apple.luong1905@gmail.com>
2019-02-21 20:14:30 +0700

    update

--- session21/mvc/18php06_mvc_oop.sql
+++ session21/mvc/18php06_mvc_oop.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 15, 2019 at 06:32 PM
--- Server version: 5.7.24-0ubuntu0.16.04.1
--- PHP Version: 7.2.13-1+ubuntu16.04.1+deb.sury.org+1
+-- Generation Time: Feb 21, 2019 at 08:14 PM
+-- Server version: 5.7.25-0ubuntu0.16.04.2
+-- PHP Version: 7.2.15-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,6 +31,17 @@ CREATE TABLE `news` (
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `title`) VALUES
+(1, 'ĐI TÌM NGUYÊN NHÂN SINH VIÊN THẤT NGHIỆP?'),
+(2, '7 ngày'),
+(5, '17 ngày'),
+(8, '7 ngày'),
+(9, 'r7');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +53,26 @@ CREATE TABLE `products` (
   `title` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL DEFAULT 'default.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'canh', '202cb962ac59075b964b07152d234b70'),
+(2, 'admin', '5a1e3a5aede16d438c38862cac1a78db');
 
 --
 -- Indexes for dumped tables
@@ -60,6 +91,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,12 +104,17 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
